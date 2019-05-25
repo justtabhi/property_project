7450656c6a541419669f54ab39a8244cdeafad97
justtabhi <abhi.in12@gmail.com>
2019-05-25 19:29:37 +0530

    first commit

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\abhii\python_web\python_proj\property_project\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ realtors/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from realtors import models
+
+class RealtorAdmin(admin.ModelAdmin):
+    list_display = ('id','name','email','hire_date')
+    list_display_links = ('id','name')
+    search_fields = ('name',)
+    list_per_page = 25 
+
+admin.site.register(
+    models.Realtor,
+    RealtorAdmin
+)
